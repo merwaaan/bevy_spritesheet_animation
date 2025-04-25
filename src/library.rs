f8d3125a0d4b0bcb10c41ca6b389860faaeed283
mgi388 <135186256+mgi388@users.noreply.github.com>
2025-04-25 10:49:20 +1000

    Use AtomicUsize to generate IDs

--- src/library.rs
+++ src/library.rs
@@ -1,6 +1,10 @@
 use std::{
     collections::{HashMap, HashSet},
-    sync::Arc,
+    sync::{
+        // TODO: Use bevy_platform when updated to Bevy 0.16.
+        atomic::{AtomicUsize, Ordering},
+        Arc,
+    },
 };
 
 use bevy::{ecs::reflect::*, prelude::Resource, reflect::prelude::*};
@@ -75,6 +79,8 @@ pub struct AnimationLibrary {
     /// Animation caches, one for each animation.
     /// They contain all the data required to play an animation.
     animation_caches: HashMap<AnimationId, Arc<AnimationCache>>,
+
+    next_id: AtomicUsize,
 }
 
 impl AnimationLibrary {
@@ -97,7 +103,7 @@ impl AnimationLibrary {
     /// ```
     pub fn register_clip(&mut self, clip: Clip) -> ClipId {
         let id = ClipId {
-            value: self.clips.len(),
+            value: self.next_id.fetch_add(1, Ordering::Relaxed),
         };
 
         self.clips.insert(id, clip);
@@ -265,7 +271,7 @@ impl AnimationLibrary {
     /// ```
     pub fn register_animation(&mut self, animation: Animation) -> AnimationId {
         let id = AnimationId {
-            value: self.animations.len(),
+            value: self.next_id.fetch_add(1, Ordering::Relaxed),
         };
 
         self.animations.insert(id, animation);
@@ -450,7 +456,7 @@ impl AnimationLibrary {
     /// ```
     pub fn new_marker(&mut self) -> AnimationMarkerId {
         let id = AnimationMarkerId {
-            value: self.markers.len(),
+            value: self.next_id.fetch_add(1, Ordering::Relaxed),
         };
 
         self.markers.insert(id);
